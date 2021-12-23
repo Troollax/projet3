f79f69846a964f53f1632193fde0c909364a2b69
BoutMan <96489393+BoutMan@users.noreply.github.com>
2021-12-23 15:48:35 +0100

    Interface 2
    Ajout des fichiers fxml, des interface AJOUTER et GALERIE

--- Projet_L2/src/Projet_L2/LaunchPage.java
+++ Projet_L2/src/Projet_L2/LaunchPage.java
@@ -4,8 +4,16 @@ package Projet_L2;
 
 
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -23,7 +31,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
-public class interfaceFX extends Application{
+public class LaunchPage extends Application{
 	
 	
 	
@@ -34,7 +42,7 @@ public class interfaceFX extends Application{
 
 	 @Override
 	 public void start(Stage stage) throws Exception {
-		 
+	
 		 
 		stage.setTitle("Bienvenue");		 
 		BorderPane root1 = new BorderPane();
@@ -54,18 +62,18 @@ public class interfaceFX extends Application{
         TreeTableView<Ouvrage> Tableau = new TreeTableView<Ouvrage>();
 
         	//Colonne Titre
-        	TreeTableColumn<Ouvrage, String> Titre = new TreeTableColumn<Ouvrage, String>("Titre");
+        	TreeTableColumn<Ouvrage, String> Titre = new TreeTableColumn<Ouvrage, String>("Titres");
 
         	//Colonne Auteur
         	TreeTableColumn<Ouvrage, String> Auteur = new TreeTableColumn<Ouvrage, String>("Auteur");
-        	TreeTableColumn<Ouvrage, String> PrenomAuteur  = new TreeTableColumn<Ouvrage, String>("Prï¿½nom");
+        	TreeTableColumn<Ouvrage, String> PrenomAuteur  = new TreeTableColumn<Ouvrage, String>("Prénom");
         	TreeTableColumn<Ouvrage, String> NomAuteur  = new TreeTableColumn<Ouvrage, String>("Nom");
         	Auteur.getColumns().addAll(PrenomAuteur, NomAuteur);
         	
         	//Colonne Nombre de mots
         	TreeTableColumn<Ouvrage, String> NbreMots = new TreeTableColumn<Ouvrage, String>("Nombre de mots");
  
-        //Dï¿½finit comment aller chercher la valeur des attributs dans la classe Ouvrage
+        //Définit comment aller chercher la valeur des attributs dans la classe Ouvrage
         Titre.setCellValueFactory(new TreeItemPropertyValueFactory<Ouvrage, String>("titre"));
         PrenomAuteur.setCellValueFactory(new TreeItemPropertyValueFactory<Ouvrage, String>("prenom"));
         NomAuteur.setCellValueFactory(new TreeItemPropertyValueFactory<Ouvrage, String>("nom"));
@@ -76,17 +84,17 @@ public class interfaceFX extends Application{
 
         //Informations
         Ouvrage un = new Ouvrage("Le petit Prince", "Antoine" , "de Saint Exuperry", 25000);
-        Ouvrage deux = new Ouvrage("L'Avare", "Moliï¿½re" , " ", 40000);
+        Ouvrage deux = new Ouvrage("L'Avare", "Molière" , " ", 40000);
         Ouvrage trois = new Ouvrage("Le Rouge et le Noir", "Stendhal" , " ", 132456);
            
 
         //Root Item
-        TreeItem<Ouvrage> itemRoot = new TreeItem<Ouvrage>(un);
-        TreeItem<Ouvrage> itemSmith = new TreeItem<Ouvrage>(deux);
-        TreeItem<Ouvrage> itemMcNeil = new TreeItem<Ouvrage>(trois);
+        TreeItem<Ouvrage> Ouvr1 = new TreeItem<Ouvrage>(un);
+        TreeItem<Ouvrage> Ouvr2 = new TreeItem<Ouvrage>(deux);
+        TreeItem<Ouvrage> Ouvr3 = new TreeItem<Ouvrage>(trois);
 
-        itemRoot.getChildren().addAll(itemSmith, itemMcNeil);
-        Tableau.setRoot(itemRoot);
+        Ouvr1.getChildren().addAll(Ouvr2, Ouvr3);
+        Tableau.setRoot(Ouvr1);
         
         StackPane root = new StackPane();
         root.setPadding(new Insets(5));
@@ -99,7 +107,7 @@ public class interfaceFX extends Application{
 		vbox2.setPadding(new Insets(10));
 		vbox2.setSpacing(40);
 		
-			//Boutons prï¿½sents dans la barre
+			//Boutons présents dans la barre
 			Button btn_Ouvrir = new Button("Ouvrir");
 			Button btn_Ajouter = new Button("Ajouter");
 			Button btn_Supprimer = new Button("Supprimer");
@@ -108,13 +116,56 @@ public class interfaceFX extends Application{
 			
 			vbox2.getChildren().addAll(btn_Ouvrir,btn_Ajouter, btn_Supprimer, btn_Editer, btn_Galerie);		
 		
-		//Positionnement des ï¿½lï¿½ments
+		//Positionnement des éléments
 		root1.setTop(hbox);
 		root1.setRight(vbox2);
 		root1.setCenter(Tableau);
 		stage.show();
 		
 		
-	}	
+		//permet d'ouvrir l'interface fxml Ajouter
+		btn_Ajouter.setOnMouseClicked((event) -> {
+		    try {
+		        FXMLLoader fxmlLoader = new FXMLLoader();
+		        fxmlLoader.setLocation(getClass().getResource("Ajouter.fxml"));
+		       
+		        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+		        Stage stage1 = new Stage();
+		        stage1.setTitle("New Window");
+		        stage1.setScene(scene);
+		        stage1.show();
+		    } catch (IOException e) {
+		        Logger logger = Logger.getLogger(getClass().getName());
+		        logger.log(Level.SEVERE, "Failed to create new Window.", e);
+		    }
+		});
+		
+		
+		
+		//Permet d'ouvrir l'interface Galerie
+		btn_Galerie.setOnMouseClicked((event) -> {
+		    try {
+		        FXMLLoader fxmlLoader = new FXMLLoader();
+		        fxmlLoader.setLocation(getClass().getResource("Galerie.fxml"));
+		       
+		        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+		        Stage stage1 = new Stage();
+		        stage1.setTitle("New Window");
+		        stage1.setScene(scene);
+		        stage1.show();
+		    } catch (IOException e) {
+		        Logger logger = Logger.getLogger(getClass().getName());
+		        logger.log(Level.SEVERE, "Failed to create new Window.", e);
+		    }
 	 
+	 
+		});
+	 
+	 
+	 }
 }
+	 
+
+
+
+
--- Projet_L2/src/module-info.java
+++ Projet_L2/src/module-info.java
@@ -8,5 +8,9 @@
 module Projet_L2 {
 	requires transitive javafx.graphics;
 	requires javafx.controls;
+	requires javafx.base;
+	requires javafx.fxml;
+	requires java.logging;
+
 	exports Projet_L2;
 }
