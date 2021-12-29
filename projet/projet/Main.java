2fa3febc57568569ce7fba3e4dd8ac2f556465f2
lokiap <72784970+lokiap@users.noreply.github.com>
2021-12-29 16:51:12 +0100

    Changement dans ConnectionBD et GestionFichier. Création de la méthode 'existe' dans ConnectionBD et migration du code concernant la base de donnée de GestionFichier(removeOuvrage) vers ConnectionBD(deleteOuvrage). A finir.

--- projet/projet/Main.java
+++ projet/projet/Main.java
@@ -21,12 +21,12 @@ public class Main {
     	
     	ConnectionBD BD = new ConnectionBD("jdbc:mysql://localhost:3307/test", "root", "usbw");
     	
-    	
+    	/*
     	GestionFichier GF = new GestionFichier();
     	Ouvrage ou = new Ouvrage("'Labelle'", "'bodelaire'", "'12/20/20021'", null, 5);
 
     	GF.removePage("'Labelle'", 0);
-    	
+    	*/
 
     }
 
